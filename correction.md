722dea41305ba74c419c8c5a0699c73f89a759a2
Etienne Rivard <etienne@kerzo.ca>
2024-11-15 13:20:21 -0500

    retroaction

---
+++ correction.md
@@ -0,0 +1,1 @@
+Dans la page principale, cartesChoisies aurait dû être un useState pour que la page se rafraîchisse à chaque fois que l'utilisateur choisit une carte.
--- src/components/Carte.tsx
+++ src/components/Carte.tsx
@@ -1,22 +1,30 @@
-import { CardMedia } from "@mui/material";
-import { useContext, useEffect, useState } from "react";
-import { CarteContext, ICarte } from "../context/carte.context";
+import { CardMedia } from '@mui/material';
+import { useContext, useEffect, useState } from 'react';
+import { CarteContext, ICarte } from '../context/carte.context';
 
 export default function Carte(props: ICarte) {
-    const [retournee, setRetournee] = useState(props.retournee);
-    const {cartesRetournee, setCarteRetournee} = useContext(CarteContext);
+  const [retournee, setRetournee] = useState(props.retournee);
+  const { cartesRetournee, setCarteRetournee } = useContext(CarteContext);
 
-    useEffect(() => {
-        setRetournee(props.retournee);
-    }, [props.retournee])
+  useEffect(() => {
+    setRetournee(props.retournee);
+  }, [props.retournee]);
 
-    const carteClique = () => {
-        setRetournee(true);
-        const nouvellesCartes = [...cartesRetournee, { id: props.id, photo: props.photo, retournee: true}];
-        setCarteRetournee(nouvellesCartes)
-    };
+  const carteClique = () => {
+    // ER: Quand est-ce que la carte est cachée? Tu la retournes, mais ne fait rien pour qu'elle se cache à nouveau.
+    setRetournee(true);
+    const nouvellesCartes = [
+      ...cartesRetournee,
+      { id: props.id, photo: props.photo, retournee: true },
+    ];
+    setCarteRetournee(nouvellesCartes);
+  };
 
-    return (
-        <CardMedia image={retournee? props.photo: "dessus-carte.svg"} sx={{ height: 150, width: 150 }} onClick={() => carteClique()} />
-    );
+  return (
+    <CardMedia
+      image={retournee ? props.photo : 'dessus-carte.svg'}
+      sx={{ height: 150, width: 150 }}
+      onClick={() => carteClique()}
+    />
+  );
 }
